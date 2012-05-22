ba1f087f45d2580cd7fe87b2dc26e3bc10650abd
pkozelka <pkozelka@92795213-5368-42fa-8248-c865abf0600f>
2012-05-22 21:00:32 +0000

    removed spring (was not used in fact), commented weld - tgz is 334M now
    git-svn-id: https://releasator.svn.sourceforge.net/svnroot/releasator/trunk@33 92795213-5368-42fa-8248-c865abf0600f

--- releasator-cli/src/net/sf/buildbox/releasator/Main.java
+++ releasator-cli/src/net/sf/buildbox/releasator/Main.java
@@ -1,8 +1,5 @@
 package net.sf.buildbox.releasator;
 
-import java.text.ParseException;
-import java.util.List;
-
 import net.sf.buildbox.args.BasicArgsParser;
 import net.sf.buildbox.args.DefaultHelpCommand;
 import net.sf.buildbox.args.annotation.AnnottationAwareSetup;
@@ -10,13 +7,10 @@ import net.sf.buildbox.releasator.legacy.CmdPrepare;
 import net.sf.buildbox.releasator.legacy.CmdPrepareAndUpload;
 import net.sf.buildbox.releasator.legacy.CmdUpload;
 import net.sf.buildbox.releasator.legacy.Params;
-import org.jboss.weld.environment.se.bindings.Parameters;
-import org.jboss.weld.environment.se.events.ContainerInitialized;
 
-import javax.enterprise.event.Observes;
-import javax.inject.Singleton;
+import java.text.ParseException;
 
-@Singleton
+//@Singleton
 public class Main {
 
     /**
@@ -33,13 +27,14 @@ public class Main {
         return BasicArgsParser.process(setup, args);
     }
 
+/*
     public void runInWeld(@Observes ContainerInitialized event, @Parameters List<String> args) throws Exception {
         System.err.println("Weld-based Releasator " + Params.releasatorVersion + " (C) 2006-2011 Petr Kozelka <pkozelka@gmail.com>");
-//        System.err.println("http://releasator.sourceforge.net");
         final AnnottationAwareSetup setup = argsSetup();
         BasicArgsParser.process(setup, args.toArray(new String[args.size()]));
     }
 
+*/
     static AnnottationAwareSetup argsSetup() throws ParseException {
         final AnnottationAwareSetup setup = new AnnottationAwareSetup("releasator");
         setup.addSubCommand(DefaultHelpCommand.class);
--- releasator-cli/src/net/sf/buildbox/releasator/UsesSpringBeans.java
+++
@@ -1,5 +0,0 @@
-package net.sf.buildbox.releasator;
-
-public interface UsesSpringBeans {
-    void useSpringBeans(SpringBeans springBeans);
-}
