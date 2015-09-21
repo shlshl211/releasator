211faf719669e8d8269fdf459ebd28a7e1c1a462
Petr Kozelka <pkozelka@gmail.com>
2015-09-21 23:54:34 +0200

    fixed author syntax: space after fullname is required

--- lib/scm-git.sh
+++ lib/scm-git.sh
@@ -9,17 +9,17 @@ function GIT_parseInfo() {
   # TODO gather USER_EMAIL, USER_FULLNAME, SCM_URLs
   USER_EMAIL=$(git config user.email)
   USER_FULLNAME=$(git config user.name)
-  echo "AUTHOR=$USER_FULLNAME<$USER_EMAIL>"
+    echo "AUTHOR=$USER_FULLNAME <$USER_EMAIL>"
   [ -z "$USER_FULLNAME" ] && return 1
   [ -z "$USER_EMAIL" ] && return 1
   return 0
 }
 
 function GIT_commit() {
-  local message="$1"
+    local message="$1"
 
-  echo "AUTHOR=$USER_FULLNAME<$USER_EMAIL>" >&2
-  git commit -am "$message" --author="$USER_FULLNAME<$USER_EMAIL>"
+    echo "AUTHOR=$USER_FULLNAME <$USER_EMAIL>" >&2
+    git commit -am "$message" --author="$USER_FULLNAME <$USER_EMAIL>"
 }
 
 function GIT_revertCommit() {
